47d76db3ac321cfb5f71ed6face9f5614047b1e0
Ghenadie Nit <evaflorn@yahoo.com>
2020-06-20 14:13:59 +0100

    Add run file

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5001, debug=True)
